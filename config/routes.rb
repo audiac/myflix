df611b42e53387171b48b4bcb42f610e6cddc5b2
Ryan <ryan.geo@gmail.com>
2015-05-15 07:55:13 -0700

    Create category show page by adding CategoriesController

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Myflix::Application.routes.draw do
   get '/home', to: 'videos#index'
 
   get '/videos/:id', to: 'videos#show', as: 'video'
+
+  get '(:genre)', to: 'categories#show', as: 'category'
 end
